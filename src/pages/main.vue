0d73b3661a3dfe337bc7d3cfea9e354639014e34
Bekzod <bekzodtoxtamuratov@gmail.com>
2024-07-30 21:18:59 +0300

    ok

--- src/pages/main.vue
+++ src/pages/main.vue
@@ -1,22 +1,16 @@
 <script setup>
-import Brandes from '../components/Brandes.vue'
 import Footer from '../components/Footer.vue'
-import Navbar from '../components/Navbar.vue'
-import NewProducts from '../components/NewProducts.vue'
-import PopularProduct from '../components/PopularProduct.vue'
-import Products from '../components/Products.vue'
-import Search from '../components/Search.vue'
 </script>
 
 <template>
-	<Navbar />
-	<Search />
+	<!-- <Navbar /> -->
+	<!-- <Search /> -->
 	<div class="bg-purple">
 		<div class="container">
-			<Products />
-			<NewProducts />
-			<Brandes />
-			<PopularProduct />
+			<!-- <Products /> -->
+			<!-- <NewProducts /> -->
+			<!-- <Brandes /> -->
+			<!-- <PopularProduct /> -->
 		</div>
 	</div>
 	<Footer />
